72c3eec919c6d60ce9673cb5c9e479c0bda23ed8
Vivek Gopinath <vivek.gopinath@sap.com>
2019-03-14 15:50:06 +0530

    changes to sh file

--- TestNG_Tutorial/Selenium_Batch.sh
+++ TestNG_Tutorial/Selenium_Batch.sh
@@ -1,4 +1,3 @@
-chmod -R 777 *
 projectLocation="$PWD"
 CLASSPATH="${projectLocation}/bin:${projectLocation}/lib/*"
 java -cp ${projectLocation}/bin:${projectLocation}/lib/* org.testng.TestNG ${projectLocation}/testng.xml
